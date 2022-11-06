1be21e618710fb608cb57eb5c53e0cb91ee89091
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2022-11-06 14:22:06 +0200

    connect to DB

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// Connect with db
+mongoose
+  .connect(process.env.DB_URI)
+  .then((connection) => {
+    console.log(
+      `Database connected successfully: ${connection.connection.host}`
+    );
+  })
+  .catch((error) => {
+    console.error(`Database connection error: ${error}`);
+    process.exit(1);
+  });
 
 // Middleware must be before Routing
 // To use middleware you use 'app.use'
