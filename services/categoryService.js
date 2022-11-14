3426d40f91a353b08f7712f1414b1fdb3df49b3f
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2022-11-14 12:40:05 +0200

    solving the error

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,16 +1,13 @@
 const CategoryModel = require("../models/categoryModel");
 const slugify = require("slugify");
 
-exports.getCategories = (req, res) => {
-  res.send();
-};
+exports.getCategories = (req, res) => {};
 
 exports.createCategory = (req, res) => {
   const name = req.body.name;
   console.log(req.body);
-  // const name = "Computers";
 
-  CategoryModel.create({ name })
+  CategoryModel.create({ name, slug: slugify(name) })
     .then((category) => res.status(201).json({ data: category }))
     .catch((error) => res.status(400).send(error));
 };
