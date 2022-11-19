32660d6d37ebd24d8ca8f544d7b4aad308ca657a
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2022-11-19 14:28:44 +0200

    add delete route

--- services/categoryService.js
+++ services/categoryService.js
@@ -16,12 +16,12 @@ exports.createCategory = asyncHandler(async (req, res) => {
 // @route     GET /api/v1/categories
 // access     Public
 exports.getCategories = asyncHandler(async (req, res) => {
-  const page = req.query.page * 1 || 1;
+  const page = req.query.page * 1 || 1; // *1 to convert the output to string.
   const limit = req.query.limit * 5 || 5;
   const skip = (page - 1) * limit;
 
   const categories = await CategoryModel.find({}).skip(skip).limit(limit);
-  res.status(200).json({ results: categories.length, data: categories });
+  res.status(200).json({ results: categories.length, page, data: categories });
 });
 
 // @desc      Get category By Id
@@ -32,7 +32,6 @@ exports.getCategory = asyncHandler(async (req, res) => {
 
   const category = await CategoryModel.findById(id);
   if (!category) {
-    // Catching error here is not working well , Check :)
     res.status(404).json({ msg: "No category for this id" });
   }
 
@@ -53,7 +52,20 @@ exports.updateCategory = asyncHandler(async (req, res) => {
   );
 
   if (!category) {
-    // Catching error here is not working well , Check :)
+    res.status(404).json({ msg: "No category for this id" });
+  }
+
+  res.status(200).json({ data: category });
+});
+
+// @desc      Delete Category By Id
+// @route     DELETE /api/v1/categories/:id
+// access     Private
+exports.deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const category = await CategoryModel.findByIdAndDelete(id);
+
+  if (!category) {
     res.status(404).json({ msg: "No category for this id" });
   }
 
